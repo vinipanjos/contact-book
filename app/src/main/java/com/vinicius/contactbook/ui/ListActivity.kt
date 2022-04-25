3a2313f51ef94830fe07e7560a887e52c28ac133
Vinicius <vinipanjos@hotmail.com>
2022-04-25 14:59:11 -0300

    Inicializando o projeto

---
+++ app/src/main/java/com/vinicius/contactbook/ui/ListActivity.kt
@@ -0,0 +1,30 @@
+package com.vinicius.contactbook.ui
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.vinicius.contactbook.databinding.ActivityMainBinding
+
+class ListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fab.setOnClickListener { view ->
+            Toast.makeText(this, "Teste clique", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
